5ae6a4f285f0a236dd75d5a671cd2e007fa3ccc5
srk400krz <srk400krz@gmail.com>
2022-06-27 15:33:20 +0900

    Working on customer info. Committed for changing file structure

--- Blog-test/src/main/java/dao/SqlDao.java
+++ Blog-test/src/main/java/dao/SqlDao.java
@@ -1,29 +1,32 @@
 package dao;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import config.DBconfig;
+import dto.Customer;
 import dto.LoginUser;
 
 public class SqlDao {
 		public final String file_path = "/Applications/Eclipse_2022-06.app/Contents/workspace/Blog-test/DBconfig.properties";
 		DBconfig config = new DBconfig();
-    
-		public List<LoginUser> check(String user, String password) throws IOException {
+		public final String file_path = "/Applications/Eclipse_2020-12.app/Contents/workspace/CustomerRepo/WebContent/DBconfig.properties";
+		//DBconfigのオブジェクト生成
+		DBconfig config = new DBconfig();
+		
+		public List<LoginUser> check(String user, String password) throws IOException{
 			String[] DbInfo = config.getDBinfo(file_path);
-
 			String url = DbInfo[0];
 			String db_user_name = DbInfo[1];
 			String db_password = DbInfo[2];
-			
-
 			String sql = "select * from login_user_tb "
 					+ "where name = ? and password = ?";
 
@@ -31,7 +34,7 @@ public class SqlDao {
 			List<LoginUser> user_info = new ArrayList<LoginUser>();
 
 			try {
-				Class.forName("com.mysql.jdbc.Driver");
+//				Class.forName("com.mysql.jdbc.Driver");
 				Connection conn = DriverManager.getConnection(url,db_user_name,db_password);
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				stmt.setString(1, user);
@@ -48,9 +51,37 @@ public class SqlDao {
 					login_user.setPassword("Not match password");
 					user_info.add(login_user);
 				}
-			} catch (SQLException | ClassNotFoundException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 			return user_info;
+		}
+		
+		public List<Customer> get_customer_info() throws FileNotFoundException{
+			String[] DbInfo = config.getDBinfo(file_path);
+			String url = DbInfo[0];
+			String db_user_name = DbInfo[1];
+			String db_password = DbInfo[2];
+			String sql = "select * from customer_tb";
+			 List<Customer> cus_info = new ArrayList<Customer>();
+			 Customer cus;
+			 try {
+//				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection(url,db_user_name,db_password);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				while(rs.next()) {
+					cus = new Customer();
+					cus.setId(rs.getInt("id"));
+					cus.setName(rs.getString("name"));
+					cus.setAddress(rs.getString("address"));
+					cus.setTel_number(rs.getString("tel_number"));
+					cus_info.add(cus);
+				}
+			 }catch(SQLException e){
+				 e.printStackTrace();
+			 }
+			 return cus_info;
+			
 		}
 }
